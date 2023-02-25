81c2d5c7381f76f247ec6556c9cd77545d6422ce
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-02-25 11:21:11 +0100

    progress card

---
+++ components/UI/Progress.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class=" w-full bg-background p-5" style="box-shadow: 0px 2px 20px rgba(79, 79, 79, 0.2);">
+        <div class=" w-full flex items-start justify-between h-full">
+            <div class=" max-w-[212px] w-full px-5 py-3 bg-white rounded-[18px] space-y-4">
+                <div class=" w-full space-y-1">
+                    <span class=" text-primary text-[18.75px] leading-[22.86px] font-montserrat font-semibold">Foto</span>
+                    <p class=" text-[9.6px] font-light font-montserrat text-primary leading-[11.7px]">
+                        Pastikan foto yang di upload memiliki Size Paling rendah 3,2 mb
+                    </p>
+                </div>
+
+                <div class=" w-full flex items-center justify-between">
+                    <button @click="Prev" class=" hover:bg-opacity-80">
+                       <IconArrowLeft />
+                    </button>
+                    <div class=" flex items-center space-x-2">
+                        <button class=" text-primary text-xs font-normal font-montserrat rounded-full border border-primary px-3 py-1 hover:text-opacity-70 hover:border-opacity-70">Skip</button>
+                        <button @click="Next" class="  text-xs font-normal font-montserrat rounded-full px-2 py-1 bg-tertiary text-white flex items-center space-x-1  hover:bg-opacity-70">
+                            <span>Next</span>
+                            <IconArrowRight />
+                        </button>
+
+                    </div>
+                </div>
+            </div>
+            <div class=" w-full h-[115px] max-w-[96px] px-5 py-3 bg-white rounded-[18px] flex">
+              <div class=" w-full flex items-center flex-col space-y-1 m-auto">
+                  <ProgressCounter :step="step" v-if="step <= 7"/>
+                  <button v-else class="  w-[50px] h-[50px] bg-tertiary rounded-full border-[2px] border-tertiary flex hover:bg-opacity-70 hover:border-opacity-80" style="box-shadow: 2px 2px 6px #F28F8F;">
+                    <span class=" m-auto text-white text-xs font-montserrat font-semibold">Next</span>
+                  </button>
+                  <span class=" text-primary font-normal text-[9.6px] font-montserrat">Complete</span>
+              </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import IconArrowLeft from '@/components/Icons/IconArrowLeft.vue'
+import IconArrowRight from '@/components/Icons/IconArrowRight.vue'
+import ProgressCounter from '@/components/UI/ProgressCounter.vue'
+export default {
+    components: {
+        IconArrowLeft,
+        IconArrowRight,
+        ProgressCounter
+    },
+    data(){
+        return {
+            step:0,
+        }
+    },
+    methods:{
+        Next(){
+            if(this.step < 8)
+             {
+                this.step += 1
+             }
+        },
+        Prev(){
+            if(this.step > 0)
+             {
+                this.step -= 1
+             }
+        }
+    }
+    
+}
+</script>
---
+++ pages/cv/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <Progress />
+</template>
+
+
+<script>
+ import Progress from '@/components/UI/Progress.vue'
+export default {
+    components: {
+        Progress
+    },
+    
+}
+</script>
