77dc04f958a2923d010821ac7d231200197d1502
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-02-27 00:36:17 +0100

    foto page

--- pages/cv/[id].vue
+++ pages/cv/[id].vue
@@ -1,10 +1,21 @@
 <template>
     <Progress />
+
+    <div class=" w-full p-5">
+        <Foto v-if="step == 1"/>
+        <div v-else>
+            <span class=" text-xl text-center font-montserrat">
+                Coming soon ...
+            </span>
+        </div>
+
+    </div>
 </template>
 
 
 <script setup>
   import Progress from '@/components/UI/Progress.vue'
+  import Foto from '@/components/Forms/Foto.vue'
   import { usesteps } from "@/composables/states";
 
   const step = usesteps();
