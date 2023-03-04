66087874c639d2aab38eba7b7f8b703c1f028369
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-03-05 00:50:34 +0100

    fixes 1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,7 +34,7 @@ export default defineNuxtConfig({
         navigateFallback: "/",
       },
       devOptions: {
-        enabled: true,
+        enabled: false,
         type: "module",
       },
     },
